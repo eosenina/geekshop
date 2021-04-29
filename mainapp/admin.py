a0a4ae275a50d4ec7ae9348df8b87190ce3397e3
Ekaterina O <eosenina@gmail.com>
2021-04-29 19:31:37 +0300

    homework3

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,3 +1,7 @@
+from mainapp.models import ProductCategory, Product
 from django.contrib import admin
 
+
 # Register your models here.
+admin.site.register(Product)
+admin.site.register(ProductCategory)
