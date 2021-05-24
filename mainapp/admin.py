b870640e13e02d40308e6150ce96b1aae2d9e50e
Ekaterina Osenina <ekaterinaosenina@imac.osenin.me>
2021-05-24 09:53:55 +0300

    homework7

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -3,5 +3,12 @@ from django.contrib import admin
 
 
 # Register your models here.
-admin.site.register(Product)
 admin.site.register(ProductCategory)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'quantity')
+    fields = ('name', 'image', 'description', ('price', 'quantity'), 'category')
+    ordering = ('name',)
+    search_fields = ('name',)
